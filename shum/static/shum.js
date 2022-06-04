f70378af43803c22c1ead4b9442e397c0c6fa326
Vol Ky <vol@wilab.org.ua>
2022-06-04 22:20:42 +0000

    what if words were buttons

--- shum/static/shum.js
+++ shum/static/shum.js
@@ -13,13 +13,40 @@ $(document.body).on('vulyk.next', (e, data) => {
     const {utt_id, transcript, wav, phones, phone_durations} = data.result.task.data;
 
     wavesurfer.load(wav.replace(/^\./, ''));
-    document.getElementById('text').innerText = transcript;
+
+    let index = 0;
+
+    // each word is a checkbox
+
+    function tokenId(loc, kind) {
+        return `_${loc.toString().padStart(3, "0")}-${kind}`;
+    }
+    function insertion(loc) {
+        return `<label class="btn btn-insertion" data-word="<unk>">
+            <input type="checkbox" autocomplete="off" name="${tokenId(loc, "I")}" value="<unk>">·
+        </label>`;
+    }
+    let buttons = `<div id="tokens" class="btn-group" data-toggle="buttons">`;
+    for (const word of transcript.split(" ")) {
+        const escaped = word.replace(/</g,'&lt;').replace(/>/g,'&gt;');
+        buttons += `
+        ${insertion(index*2)}
+        <label class="btn btn-correct active">
+            <input type="checkbox" autocomplete="off" checked name="${tokenId(index*2+1, "S")}" value="${word}">${escaped}</input>
+        </label>
+        `;
+        index += 1;
+    }
+    buttons += `${insertion(index*2)}</div>`;
+    document.getElementById('hypothesis').innerHTML = buttons;
+
     document.forms.shum.utt_id.value = utt_id;
-    document.forms.shum.transcript.value = transcript;
+    document.forms.shum.hypothesis.value = transcript;
 })
 
 $(document.body).on('vulyk.save', (e, callback) => {
-    const value = Object.fromEntries((new FormData(document.forms.shum)).entries())
+    const value = Object.fromEntries((new FormData(document.forms.shum)).entries());
+    console.log(value);
     callback(value);
 });
 
